701ce05c50808c6fcd98ad61787bf0af6257ccc2
David Härer <david@haerer.dev>
2023-05-12 16:44:33 +0200

    antiphish.js

--- antiphish.js
+++ antiphish.js
@@ -1,8 +1,6 @@
 const links = document.getElementsByTagName("a");
 const base = new URL(window.location.origin);
 
-const flagged = [];
-
 function warning(url) {
   return `You are about to visit:\n${url}\n\nThis is a Unicode link which is often used for phishing!`;
 }
